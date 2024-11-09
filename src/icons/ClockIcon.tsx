a38424df4b7d3f693c6f2c92f658fa5819a462c1
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-11-09 20:00:34 -0300

    clock and arrow back icons

---
+++ src/icons/ClockIcon.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react';
+import { IconInterface } from './IconInterface';
+
+export const ClockIcon = forwardRef<SVGSVGElement, IconInterface>(({ iconColor = "currentColor", size = "24px" }, ref) => {
+    return (
+        <svg
+            ref={ref}
+            width={size}
+            height={size}
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M12.4998 6.5V12.5L8.33317 14.5M2.08317 12.5C2.08317 18.0228 6.74687 22.5 12.4998 22.5C18.2528 22.5 22.9165 18.0228 22.9165 12.5C22.9165 6.97715 18.2528 2.5 12.4998 2.5C6.74687 2.5 2.08317 6.97715 2.08317 12.5Z"
+                stroke={iconColor}
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+});
--- src/stories/Icons.stories.tsx
+++ src/stories/Icons.stories.tsx
@@ -8,6 +8,8 @@ import { BagIcon } from "@/icons/BagIcon";
 import { RecipeIcon } from "@/icons/RecipeIcon";
 import { BasketIcon } from "@/icons/BasketIcon";
 import { HeartIcon } from "@/icons/HeartIcon";
+import { ArrowBackIcon } from "@/icons/ArrowBackIcon";
+import { ClockIcon } from "@/icons/ClockIcon";
 
 type StoryProps = {
     iconColor: string;
@@ -314,3 +316,36 @@ export const HeartIconVariant2: Story = {
     },
 };
 
+export const ArrowBackIconVariant1: Story = {
+    args: {
+        iconColor: 'black',
+        size: '32px',
+        variant: 'variant1',
+    },
+    render: (args) => {
+        return (
+            <ArrowBackIcon
+                iconColor={args.iconColor}
+                size={args.size}
+                variant={args.variant}
+            />
+        );
+    },
+};
+
+export const ClockIconVariant1: Story = {
+    args: {
+        iconColor: 'black',
+        size: '32px',
+        variant: 'variant1',
+    },
+    render: (args) => {
+        return (
+            <ClockIcon
+                iconColor={args.iconColor}
+                size={args.size}
+                variant={args.variant}
+            />
+        );
+    },
+};
