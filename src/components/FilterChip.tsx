4525dd27392a66ab61ae698964d7aa7703353fe9
totob <tberger@mail.austral.edu.ar>
2024-11-11 15:57:59 -0300

    fliterchip añadido

--- src/components/FilterChip.tsx
+++ src/components/FilterChip.tsx
@@ -2,7 +2,6 @@
 import { ButtonHTMLAttributes, forwardRef } from "react";
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "../utils/UtilFunctions.ts";
-import CheckIcon from "../icons/CheckIcon"; // Asegúrate de ajustar la ruta si es necesario
 
 interface FilterChipProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof chipVariants> {
   label: string;
@@ -14,7 +13,7 @@ const chipVariants = cva(
     variants: {
       variant: {
         default: "bg-primary-50 font-roboto text-gray-900 shadow-md", // Fondo y texto para default
-        selected: "bg-primary-100 font-roboto text-gray-900 shadow-lg", // Fondo y texto para selected
+        selected: "bg-primary-100  text-gray-900 shadow-lg", // Fondo y texto para selected
       },
     },
     defaultVariants: {
@@ -32,7 +31,16 @@ const FilterChip = forwardRef<HTMLButtonElement, FilterChipProps>(
         {...props}
       >
         {variant === "selected" && (
-          <CheckIcon /> // Usa tu componente de icono personalizado
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white"
+            strokeWidth="4" 
+            className="w-4 h-4 mr-1"
+          >
+            <path strokeLinecap="square" strokeLinejoin="miter" d="M5 13l4 4L19 7" />
+          </svg>
         )}
         {label}
       </button>
