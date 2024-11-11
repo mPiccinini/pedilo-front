4da5849c48216a02f3f5290656b67649e7922044
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-11-11 19:29:13 -0300

    cambio en el tag

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -8,7 +8,7 @@ interface TagProps {
 const Tag: React.FC<TagProps> = ({ text, variant = 'variant1' }) => {
   const baseClasses = 'rounded-full inline-flex items-center justify-center text-xs px-2 py-1';
   const variantClasses = variant === 'variant2'
-    ? 'border border-tertiary-500 rounded-[8px] bg-tertiary-50 font-roboto font-bold text-tertiary-950'
+    ? 'border border-tertiary-500 rounded-[9px] bg-tertiary-50 font-roboto font-bold text-tertiary-950'
     : 'bg-[#EBFCFF] text-black';
 
   return (
