770469b0350660b4c04f2a7fb07bee8065d8040b
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-11-11 18:37:06 -0300

    tagFoodOrigin

---
+++ src/stories/TagFoodOrigin.stories.tsx
@@ -0,0 +1,16 @@
+// TagFoodOrigin.stories.tsx
+import { Meta, Story } from '@storybook/react';
+import TagFoodOrigin, { TagFoodOriginProps } from '@/components/TagFoodOrigin';
+
+export default {
+    title: 'Components/TagFoodOrigin',
+    component: TagFoodOrigin,
+} as Meta;
+
+const Template: Story<TagFoodOriginProps> = (args) => <TagFoodOrigin {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    text: 'Italiana',
+    imageSrc: 'images/comidaitaliana.png',
+};
