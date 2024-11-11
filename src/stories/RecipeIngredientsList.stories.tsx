6eb3b75c89247e14be686caed7bcebdcd0398d8b
alexk <akottner@mail.austral.edu.ar>
2024-11-11 12:59:41 -0300

    done ingredients list

---
+++ src/stories/RecipeIngredientsList.stories.tsx
@@ -0,0 +1,32 @@
+import RecipeIngredientsList from "@/components/RecipeIngredientsList";
+import CheckBoxIcon from "@/icons/CheckBoxIcon";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: 'Components/RecipeIngredientsList',
+    component: RecipeIngredientsList,
+    argTypes: {
+        ingredients: {
+            control: { type: 'array' },
+            description: 'Lista de ingredientes',
+            defaultValue: ['Tomate', 'Lechuga', 'Cebolla'],
+        },
+        prices: {
+            control: { type: 'array' },
+            description: 'Lista de precios de los ingredientes',
+            defaultValue: [100, 200, 300],
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RecipeIngredientsList>;
+
+export const Default : Story = {
+    args: {
+        ingredients: ['Tomate', 'Lechuga', 'Cebolla'],
+        prices: [100, 200, 300],
+    },
+    render: (args) => <RecipeIngredientsList {...args} />,
+};
