a081f56bfdc0d3010aa49e41b50b66fd4c6fc081
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-11-11 14:32:53 -0300

    new changes

--- src/components/ResumenDeCompra.tsx
+++ src/components/ResumenDeCompra.tsx
@@ -12,28 +12,24 @@ const ResumenDeCompra: React.FC<ResumenDeCompraProps> = ({
   footerTitles,
 }) => {
   return (
-    <div className="w-[361px] h-[226px] rounded-lg border border-gray-300 p-4 shadow-md bg-white flex flex-col justify-between">
-      <div className="flex justify-between px-4">
-        {/* Labels de la izquierda */}
-        <div className="space-y-2">
+    <div className="w-[361px] h-[226px] rounded-lg border border-gray-300 p-4 shadow-md bg-white flex flex-col">
+      <div className="flex justify-between w-full space-x-6 mt-4 px-6">
+        <div className="space-y-0">
           {labelsLeft.slice(0, 3).map((label, index) => (
             <p key={index} className="text-label1">{label}</p>
           ))}
         </div>
-
-        {/* Labels de la derecha */}
-        <div className="space-y-2 text-right">
+        <div className="space-y-0 text-right">
           {labelsRight.slice(0, 3).map((label, index) => (
             <p key={index} className="text-label1">{label}</p>
           ))}
         </div>
       </div>
 
-      {/* Separador */}
-      <div className="border-t border-black my-20 mx-[-16px] w-[90%] mx-auto"></div>
+      
+      <div className="border-t-2 border-black w-[90%] mx-auto my-2 rounded-full"></div> 
 
-      {/* Footer titles */}
-      <div className="flex justify-between px-0 mt-0 pb-0">
+      <div className="flex justify-between w-full px-6 pt-0"> 
         <p className="font-semibold text-sm">{footerTitles[0]}</p>
         <p className="font-semibold text-sm">{footerTitles[1]}</p>
       </div>
--- src/stories/ResumenDeCompra.stories.tsx
+++ src/stories/ResumenDeCompra.stories.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import ResumenDeCompra from '@/components/ResumenDeCompra';
 
-// Configuración de Storybook para el componente
 export default {
   title: 'Components/ResumenDeCompra',
   component: ResumenDeCompra,
@@ -13,10 +11,8 @@ export default {
   },
 } as ComponentMeta<typeof ResumenDeCompra>;
 
-// Plantilla para crear historias
 const Template: ComponentStory<typeof ResumenDeCompra> = (args) => <ResumenDeCompra {...args} />;
 
-// Ejemplo de historia con datos de ejemplo
 export const Default = Template.bind({});
 Default.args = {
   labelsLeft: ['Label1', 'Label1', 'Label1', 'Label1'],
